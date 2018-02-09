554176223fa026b73ccdf104bf51a38b22ea9cda
passakun <passakun@thinknet.co.th>
2018-02-09 14:02:04 +0700

    Edit hello function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const moment = require('moment');
+const hello = r
 
-module.exports = name => `Hello ${name} at ${moment().format('MM/DD/YYYY')}`
+module.exports = name => `Hello ${name} at ${moment().format('MM/DD/YYYY HH:MM:ss')}`
